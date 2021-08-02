f3c2bc6da495a72cbd085afc42bd936b46c0422e
James Tutton <james@comexe.co.uk>
2021-08-02 15:01:10 +0100

    Opps

--- build/proxmox/build7.sh
+++ build/proxmox/build7.sh
@@ -3,7 +3,7 @@ set -e
 
 export PVE_KERNEL_BRANCH=master
 export RELAX_INTEL_GIT_REPO="https://github.com/jamestutton/relax-intel-rmrr.git"
-export PROXMOX_PATCH="add-relaxable-rmrr-5_11.patch"
-export RELAX_PATCH="proxmox7.patch"
+export RELAX_PATCH="add-relaxable-rmrr-5_11.patch"
+export PROXMOX_PATCH="proxmox7.patch"
 
 ./build.sh
