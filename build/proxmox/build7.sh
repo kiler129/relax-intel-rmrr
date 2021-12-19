f61cf36ecf1fa7b97065cbe08cb161c3a810ef59
Michael Trip <m.trip@atcomputing.nl>
2021-12-19 21:08:18 +0100

    pushed kernel version to 5.13

--- build/proxmox/build7.sh
+++ build/proxmox/build7.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-export PVE_KERNEL_BRANCH=pve-kernel-5.11
+export PVE_KERNEL_BRANCH=pve-kernel-5.13
 export RELAX_INTEL_GIT_REPO="https://github.com/jamestutton/relax-intel-rmrr.git"
 export RELAX_PATCH="add-relaxable-rmrr-5_11.patch"
 export PROXMOX_PATCH="proxmox7.patch"
